259f8de840d301a23a2ecf27c22c27dc8ab28d85
JustinDEvasia116 <djst.devasia@gmail.com>
2022-11-25 18:57:44 +0530

    dp

--- ZAP_ONLINE/settings.py
+++ ZAP_ONLINE/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-xv(5#qufq6a=8!%=y5_$&-s(a^6alo4tadnpo&jr8q+gac9fx^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
