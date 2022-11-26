1ca4f7e57334c82523b85af6b19abc567f7cc74a
JustinDEvasia116 <djst.devasia@gmail.com>
2022-11-26 15:23:19 +0530

    deploy7

--- ZAP_ONLINE/settings.py
+++ ZAP_ONLINE/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 
